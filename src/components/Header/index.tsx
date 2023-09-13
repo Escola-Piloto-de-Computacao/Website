5db69b60388232bcb33b4af0afa6641968b635a4
Wender Enzo <wenderenzo2014@outlook.com>
2023-09-13 20:06:36 -0300

    feat. insert begginer landin-page

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+const Header = () => {
+    return (
+        <header className="flex items-center justify-between mx-6 my-4">
+          <div>
+            <img src="/Logo.svg" alt="" />
+          </div>
+          <div className="flex gap-7 font-semibold text-xl">
+            <a href="" className="hover:text-gray-500">Home</a>
+            <a href="" className="hover:text-gray-500">Sobre nós</a>
+            <a href="" className="hover:text-gray-500">Eventos</a>
+            <a href="" className="hover:text-gray-500">Membros</a>
+          </div>
+      </header>
+    );
+}
+export default Header;
