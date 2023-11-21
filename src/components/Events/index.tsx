8eef424a5bf1f2aaa1094482c5ab2bf753b0e9d6
Pedro <pedrofigueiraboaviagem@gmail.com>
2023-11-21 19:50:21 -0300

    import fixes

--- src/components/Events/index.tsx
+++ src/components/Events/index.tsx
@@ -1,4 +1,6 @@
 import Card from './Card';
+import capcomlogo from '../../Assets/CapcomLogo.png';
+import wtcclogo from '../../Assets/WTCClogo.svg';
 
 //TODO fix the websites links
 
@@ -6,19 +8,19 @@ const eventos = [
    {
       NameEvent: "CAPCOM",
       DescriptionEvent: "A Semana de Capacitação em Computação da Ufersa promove minicursos de extensão para alunos da UFERSA. Os cursos abrangem tecnologias essenciais, ministrados em sua maioria por alunos do curso. As aulas acontecerão de 29 de novembro a 01 de dezembro, das 14:00PM às 18:00PM, visando complementar a formação dos estudantes. Os tópicos incluem Web Development, Unity, Git, Laravel, PowerBI, Python, R, e Robótica. Essa iniciativa visa enriquecer o conhecimento dos alunos e proporcionar experiência em ensino. Junte-se a nós para adentrar mais ainda no mundo de ciência da computação!",
-      ImageEvent: "/capcomLogo.png",
+      ImageEvent: capcomlogo,
       EventSiteLink: "https://google.com",
    },
    {
       NameEvent: "WTCC IX",
       DescriptionEvent: "O Workshop Técnico-Científico de Computação visa despertar e estimular o interesse dos estudantes de graduação e de Pós-Graduação em Computação na pesquisa científica sobre temas relacionados à ciência da computação. Assim como discutir temas atuais da área que são importantes tanto para estudantes quanto para profissionais de computação.",
-      ImageEvent: "/Cardimage.svg",
-      EventSiteLink: "https://bing.com",
+      ImageEvent: wtcclogo,
+      EventSiteLink: "",
    },
    /*{
       NameEvent: "2º Campeonato de LOL",
       DescriptionEvent: "em breve...",
-      ImageEvent: "/Cardimage.svg",
+      ImageEvent: "",
       EventSiteLink: "https://bing.com",
    }*/
 ]
