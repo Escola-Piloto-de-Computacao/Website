946e75eebd69384d96db208bcbd020da9019c243
Pedro <pedrofigueiraboaviagem@gmail.com>
2023-11-22 16:14:13 -0300

    fixes

--- src/components/Introduction/index.tsx
+++ src/components/Introduction/index.tsx
@@ -15,7 +15,7 @@ const Introduction = () => {
                <p className="text-lg mt-4 w-5/6 text-justify">As Escolas Piloto (EP) são iniciativas de extensão realizadas por estudantes e professores de universidades brasileiras. Esses projetos buscam preencher lacunas de conhecimento que ainda não são abordadas nos currículos tradicionais das instituições de Ensino Superior do país. A EPC busca sintonizar os estudantes com o que há de mais atual na área da computação. Isso correrá por meio de produção de minicursos, organização de palestras com profissionais da área, mesas redondas, treinamentos internos e externos, e pela realização de visitas técnicas.</p>
             </div>
             <div className="lg:w-5/6 flex justify-center">
-               <button className="bg-sky-500 text-white rounded-md mt-8 py-2 w-60" onClick={handleButtonClick}>Veja nossos eventos!</button>
+               <button className="bg-sky-500 text-white rounded-md mt-8 py-2 w-60 text-lg lg:text-base" onClick={handleButtonClick}>Veja nossos eventos!</button>
             </div>
          </div>
          <div className="lg:col-span-6 hidden lg:flex justify-center items-center">
